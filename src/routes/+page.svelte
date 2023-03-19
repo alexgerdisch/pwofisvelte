7ec442bd377670a05984d5aae3a057b65f9b320a
alexgerdisch <alexgerdisch@gmail.com>
2023-03-18 23:11:20 -0700

    added firebase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
 <script>
+    import { initializeApp } from "firebase/app";
+    import { getAuth,
+            createUserWithEmailAndPassword,
+            signInWithEmailAndPassword,
+            signOut } from "firebase/auth";
+    
     import Button from "./Button.svelte";
     import ActionBar from "./ActionBar.svelte";
     import RegisterForm from "./RegisterForm.svelte";
     import LoginForm from "./LoginForm.svelte";
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyDL9Vfw5AJKfmeeXFRlFuyMKvH16C8lyGk",
+        authDomain: "pwofibeta.firebaseapp.com",
+        projectId: "pwofibeta",
+        storageBucket: "pwofibeta.appspot.com",
+        messagingSenderId: "612729901997",
+        appId: "1:612729901997:web:cbfddce6f2bf647eb20724"
+    };
+    
+    //Init firebase app
+    const app = initializeApp(firebaseConfig);
+    console.log(app);
+
+
+
+
 </script>
 
 <h1>Welcome to Pwofi</h1>
