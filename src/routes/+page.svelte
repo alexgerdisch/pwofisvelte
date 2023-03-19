ebb26df370548d8e8e6ec4e6cef68c03d026a0d4
alexgerdisch <alexgerdisch@gmail.com>
2023-03-18 21:07:43 -0700

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Button from "./Button.svelte";
+    import ActionBar from "./ActionBar.svelte";
+</script>
+
+<h1>Welcome to Pwofi</h1>
+<p>
+    As you can see, we are currently rough around the edges, but it's what's on
+    the inside that counts.
+</p>
+
+<Button name="Sign In" />
+<Button name="Sign Out" />
+
+<ActionBar />
---
+++ src/routes/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let name;
+
+    const signIn = () => {};
+</script>
+
+<button>{name}</button>
+
+<form>
+    <input type="text" class="" />
+</form>
