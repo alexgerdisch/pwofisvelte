d3c3325ab1bb2b3469160d7c93f8d55864941955
alexgerdisch <alexgerdisch@gmail.com>
2023-06-07 21:43:58 -0700

    session saveToFirestore()

--- src/lib/util/session.js
+++ src/lib/util/session.js
@@ -1,4 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
+// import { writable } from "svelte/store";
+
+// export const activeSession = writable({});
 
 export class Session {
     constructor(type) {
@@ -6,4 +9,12 @@ export class Session {
         this.seshId = uuidv4();
         this.convo = [];
     }
+
+
+    // method that saves session details to user's Firestore
+
+    saveToFirestore() {
+        
+    }
+
 }
